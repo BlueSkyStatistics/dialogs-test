da6b88803a2dc00c4e919afd70469612b66bc90a
adpgithub <anildabralpro@gmail.com>
2023-12-08 14:47:15 +0530

    BSky R v8.89

--- Example1.js
+++ Example1.js
@@ -8,8 +8,8 @@
 
 var localization = {
     en: {
-        title: "Example1-1.1.4",
-        navigation: "Example1-1.1.4",
+        title: "Example1-1.1.7",
+        navigation: "Example1-1.1.7",
         modelname: "Enter Model Name",
         dependent: "Dependent variable",
         independent: "Independent variable(s)",
