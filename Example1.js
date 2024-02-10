e41713d573ca50e20737c63223000e8420397f20
adpgithub <anildabralpro@gmail.com>
2024-02-10 21:07:59 +0530

    ver 8.96

--- Example1.js
+++ Example1.js
@@ -8,8 +8,8 @@
 
 var localization = {
     en: {
-        title: "Example1-1.3.9",
-        navigation: "Example1-1.3.9",
+        title: "Example1-1.4.0",
+        navigation: "Example1-1.4.0",
         modelname: "Enter Model Name",
         dependent: "Dependent variable",
         independent: "Independent variable(s)",
